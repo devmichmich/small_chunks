cdecbf9d77d495b2fecf080a4251a6b6bfed772e
devmichmich <77087061+devmichmich@users.noreply.github.com>
2022-07-21 07:06:03 +0100

    add double stack queue for multithreading code

--- multithreading.py
+++ multithreading.py
@@ -1,3 +1,19 @@
-#comment test
-print ("printing via printer")
-print ("printing from vscode")
+class DoubleStackQueue:
+
+    def __init__(self):
+        self.stac = []
+        self.stac2 =[]
+
+    def push(self, x: int) -> None:
+        self.stac.append(x)
+
+    def pop(self) -> int:
+        while len(self.stac) != 1:
+            self. stac2.append(self.stac.pop())
+        ans = self.stac[0]
+        self. stac.pop()
+        while len(self.stac2) != 0:
+            self.stac.append(self.stac2.pop())
+        return ans
+    def peek(self) -> int:
+        return self.stac[0]
